bb66deb32d65f407fe61e2cdb77537ed4aab7074
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-05-26 19:40:01 -0300

    fix: remove commented code

--- TapGame/ViewModel/ViewModel.swift
+++ TapGame/ViewModel/ViewModel.swift
@@ -11,14 +11,9 @@ class ViewModel: ObservableObject {
 
     @Published var selectedImgIndex = 0
     @Published var targetIndex = 1
-    // @Published var isPlayViewPresented = false
-    // @Published var isResultViewPresented = false
     @Published var activeScreen: Screen = .start
     @Published var failed = false
     @Published var score = 0
-    // @Published var alertTitle = ""
-    // @Published var alertMessage = ""
-    // @Published var isAlertVisible = false
 
     var timer = Timer.publish(every: 1, on: .main, in: .common)
     var cancellable: Cancellable?
